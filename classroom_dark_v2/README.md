6573bee3db728018d83232f2733e6999cfbce1b8
Andy <105518328+DeltAndy123@users.noreply.github.com>
2023-12-21 16:42:28 +0000

    Add Classroom Dark V2, use Bun instead of Node.js, use commit action

---
+++ classroom_dark_v2/README.md
@@ -0,0 +1,9 @@
+# Google Classroom Dark Theme V2
+
+I decided to create my own dark theme for Google Classroom because Dark Reader isn't perfect, and there is no good style that has the following:
+- Currently works
+- Doesn't interfere with class accent color
+- Has enough contrast to read
+
+The [old style](<../Google Classroom Dark Theme/README.md>)'s code had a lot of unneccesary stuff, was old, and broke with a UI update,
+so I rewrote the entire theme from scratch. This version has more customizations.
--- update.js
+++ update.js
@@ -2,28 +2,50 @@ const stylus = require('stylus');
 const axios = require('axios');
 const fs = require('fs');
 
-/** Steps for Google Classroom Dark Theme:
- * 1. Fetch the UserStyles.world CSS from "https://userstyles.world/api/style/1728.user.css"
- * 2. Save the file to "./Google Classroom Dark Theme/classroom-dark.user.css"
- * 3. Parse the Stylus CSS into vanilla CSS and save it to "./Google Classroom Dark Theme/classroom-dark.css"
- */
-
+// Steps for Google Classroom Dark Theme:
+// 1. Fetch the UserStyles.world CSS from "https://userstyles.world/api/style/1728.user.css"
+// 2. Save the file to "./Google Classroom Dark Theme/classroom-dark.user.css"
+// 3. Parse the Stylus CSS into vanilla CSS and save it to "./Google Classroom Dark Theme/classroom-dark.css"
 axios
   .get('https://userstyles.world/api/style/1728.user.css')
   .then((response) => {
     fs.writeFileSync('./Google Classroom Dark Theme/classroom-dark.user.css', response.data);
     
     var stylusCss = response.data
-      .replace(/@-moz-document regexp\("\^\(https\?\):\/\/ogs\.google\.\*"\)\s+{(.|\n)*/gm, '') // Remove the second @-moz-document block
-      .replace(/(?:\/\* ==UserStyle==(\n|.)*?(?=@var)|==\/UserStyle== \*\/)/gm, '') // Remove the extra unneeded meta
-      .replace(/@var\s+\w+\s+(\w+)\s+".*?"\s+(.*)/gm, '$1 = $2;') // Replace @var with variable assignment
-      .replace(/@-moz-document domain\("classroom\.google\.com"\) {((?:\n|.)*)}/gm, '$1') // Remove the @-moz-document block but keep the CSS
+      .replace(/@-moz-document regexp\("\^\(https\?\):\/\/ogs\.google\.\*"\)\s*{[\S\s]*}/, '') // Remove the second @-moz-document block
+      .replaceAll(/(?:\/\* ==UserStyle==[\S\s]*?(?=@var)|==\/UserStyle== \*\/)/gm, '') // Remove the extra unneeded meta
+      .replaceAll(/@var\s+\w+\s+([\w-]+)\s+".*?"\s+(.*)/gm, '$1 = $2;') // Replace @var with variable assignment
+      .replaceAll(/@-moz-document domain\("classroom\.google\.com"\) {([\S\s]*)}/gm, '$1') // Remove the @-moz-document block but keep the CSS
+      
+    fs.writeFileSync('./Google Classroom Dark Theme/classroom-dark.styl', stylusCss)
 
     stylus(stylusCss)
       .render((err, css) => {
         if (err) throw err;
 
         fs.writeFileSync('./Google Classroom Dark Theme/classroom-dark.css', css);
+
+      });
+  });
+
+// Same thing for Classroom Dark V2, but ID is 12025
+axios
+  .get('https://userstyles.world/api/style/12025.user.css')
+  .then((response) => {
+    fs.writeFileSync('./classroom_dark_v2/classroom-dark.user.css', response.data);
+    
+    var stylusCss = response.data
+      .replaceAll(/(?:\/\* ==UserStyle==[\S\s]*?(?=@var)|==\/UserStyle== \*\/)/gm, '') // Remove the extra unneeded meta
+      .replaceAll(/@var\s+\w+\s+([\w-]+)\s+".*?"\s+(.*)/gm, '$1 = $2;') // Replace @var with variable assignment
+      .replaceAll(/@-moz-document domain\("classroom\.google\.com"\) {([\S\s]*)}/gm, '$1') // Remove the @-moz-document block but keep the CSS
+
+    fs.writeFileSync('./classroom_dark_v2/classroom-dark.styl', stylusCss)
+
+    stylus(stylusCss)
+      .render((err, css) => {
+        if (err) throw err;
+
+        fs.writeFileSync('./classroom_dark_v2/classroom-dark.css', css);
 
       });
   });
